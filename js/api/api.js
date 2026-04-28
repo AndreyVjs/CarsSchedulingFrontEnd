d9d92f8707ddd1ec1e2731351dccfcde71c18b19
andrey_vjs <andreyvjs@gmail.com>
2026-04-28 16:05:10 -0300

    I used Antigravity to do the basic HTML and CSS layout of the pages; now I'm going to work on correcting its errors, defining the IDs in the tags, and start working on the dynamism with JavaScript and using the resources of my API.

--- js/api/api.js
+++ js/api/api.js
@@ -1,31 +1,58 @@
-const getAllCustomers = 'http://localhost:3000/customer/all';
+//Esse arquivo é responsavel por gerenciar todas as request da APi CarScheduling
 
+//GetsAll
+const getAllCustomers = 'http://localhost:3000/customer/all';
+const getAllEmployee = '';
+const getAllCars = '';
+const getAllServices = '';
+const getAllWorkOrder = '';
+
+//GetsByID
+const getCustomerById = 'http://localhost:3000/customer/';
+const getEmployeeById = 'http://localhost:3000/employee/';
+const getCarById = 'http://localhost:3000/car/';
+const getWorkOrderById = '';
+const getServiceById = '';
+
+//Posts
+const postCustomer = '';
+
+//Lista completa de Customer da tabela do banco
 async function getCustomers(){
 
     let promise = await fetch(getAllCustomers);
     let customersJson = await promise.json();
-    console.log(customersJson);
-
-    console.log(customersJson[0]);
+    return customersJson; 
 }
 
-// getCustomers();
 
-async function CustomerById(id) {
+// Retorna um Customer especifico da tabela pelo id passado
+async function customerById(id) {
 
-    let promise = await fetch(`http://localhost:3000/customer/${id}`);
+    let promise = await fetch(getCustomerById+'${id}');
     let customerJson = await promise.json();
-    console.log(customerJson);
+    return customerJson;
 }
 
-// CustomerById(1);
 
+// Retorna um Employee especifico da tabela pelo id passado
+async function employeeById(id) {
 
-async function getEmployeeById(id) {
-
-    let promise = await fetch(`http://localhost:3000/employee/${id}`);
+    let promise = await fetch(getEmployeeById+'${id}');
     let employeeJson = await promise.json();
-    console.log(employeeJson);
+    return employeeJson;
 }
 
-getEmployeeById(4);
+
+// Retorna um Carro especifico da tabela pelo id passado
+async function carById(id){
+
+    let promise = await fetch(getCarById+'${id}');
+    let carJson = await promise.json();
+    return carJson;
+}
+
+//Chama o post de Work Order e cadastra a ordem
+export async function createWorkOrderApi(idCar, idCustomer, idsService) {
+    
+}
--- pages/index/script.js
+++ pages/index/script.js
@@ -1,5 +1,5 @@
 // let modelo = document.querySelector(".texts");
-
+import {createWorkOrder} from 'js/api/api.js'
 
 function adicionarTexto(){
 
@@ -18,13 +18,32 @@ function adicionarTexto(){
     return alert('Texto adicionado com sucesso!');
 }
 
+
 function createWorkOrder(){
 
     //Pegar dados do input
-    let id_car = document.getElementById('car-input').value;
-    let ids_service = document.getElementById('services-input').value;
+    
+    //let idCar = document.getElementById('car-input').value;
+    //let idsService = document.getElementById('services-input').value;
+    //let idCustomer = document.getElementById('customer-input').value;
+
+    let idCar = document.querySelector('#car-input').value;
+    let idsService = document.querySelector('#services-input').value;
+    let idCustomer = document.querySelector('#customer-input').value;
+
+    if (idCar || idsService || idCustomer){
+
+        //Chamar api e passar os dados
+        //Seria interessante passar logo o JSON
+        createWorkOrder(idCar, idCustomer, idsService);
+        } else {
+        alert("Preencha todos os campos!!")
+    }
 
 }
 
 let getStartedButton = document.getElementById('started-button');
 getStartedButton.addEventListener('click', adicionarTexto);
+
+let createWorkOrderButton = document.getElementById().value;
+createWorkOrderButton.addEventListener('click', createWorkOrder)
